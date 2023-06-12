ae84078d39422208b2f3bfb0d206c7e63ce35fe3
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-06-12 08:10:13 -0700

    basic file structure

---
+++ ryan-react-portfolio/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Resume from "./components/pages/Resume";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+import NavTabs from "./components/NavTabs";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <NavTabs />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
