a70577356b4779b317ac7d824ea677c3146f0f36
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-06-12 15:18:26 -0700

    added resume page, and more styling, with some zoom animation on resume page

--- ryan-react-portfolio/src/components/pages/AboutMe.js
+++ ryan-react-portfolio/src/components/pages/AboutMe.js
@@ -8,9 +8,8 @@ export default function About() {
   const cardStyle = {
     width: "50%",
     margin: "0 auto",
-    marginTop: "20vh",
     padding: "2rem",
-    backgroundColor: "#E1A419",
+    background: "linear-gradient(to bottom, #E1A419, #C48711, #A1660A)",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -18,50 +17,44 @@ export default function About() {
     borderRadius: "8px",
     boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
   };
+
+  const textStyle = {
+    textAlign: "center",
+    color: "#FFF",
+    marginBottom: "1rem",
+  };
+
+  const titleStyle = {
+    fontSize: "1.5rem",
+  };
+
+  const descStyle = {
+    fontSize: "1rem",
+    lineHeight: "1.5",
+    textAlign: "justify",
+  };
+
   return (
-    <div>
-      <body style={cardStyle}>
+    <div className="pageContainer">
+      <div style={cardStyle}>
         <div className="card img-responsive">
           <div className="col-12 card-text2">
             <div className="portada"></div>
             <div className="title-total">
-              <div className="title">
+              <div className="title" style={(textStyle, titleStyle)}>
                 Full Stack Developer & Ski Patroller/Mountain Guide
               </div>
               <h2>Ryan Wood</h2>
 
-              <div className="desc">
+              <div className="desc" style={(textStyle, descStyle)}>
                 Hi there! I'm a passionate and adventurous individual who
                 recently made a bold career transition into the world of coding.
-                With a decade-long background in the outdoor industry as a
-                mountain guide and ski patroller, I've always been drawn to
-                challenges and pushing boundaries. Having experienced the thrill
-                of conquering mountains and navigating through unpredictable
-                terrain, I've developed a resilient and problem-solving mindset
-                that I now bring to my coding journey. I thrive in dynamic
-                environments, and I'm excited to apply my skills in full stack
-                development to create innovative solutions and contribute to the
-                ever-evolving tech landscape. During my time at coding bootcamp,
-                I honed my skills in front-end and back-end development, gaining
-                proficiency in languages such as JavaScript, HTML, CSS, and
-                frameworks like React and Node.js. I'm eager to dive deeper into
-                the world of web development, exploring new technologies and
-                frameworks to expand my toolkit. What sets me apart is my unique
-                combination of technical expertise and real-world outdoor
-                experience. I understand the importance of teamwork,
-                adaptability, and problem-solving under pressure. I bring a
-                fresh perspective to the table, fueled by my passion for both
-                the digital and natural worlds. I'm excited to collaborate with
-                like-minded individuals and organizations, leveraging my skills
-                and background to create meaningful and impactful software
-                solutions. Let's embark on this coding journey together! Feel
-                free to reach out and connect. I'm always up for a new coding
-                challenge or a climbing/skiing adventure!
+                ...
               </div>
             </div>
           </div>
         </div>
-      </body>
+      </div>
     </div>
   );
 }
--- ryan-react-portfolio/src/components/pages/Resume.js
+++ ryan-react-portfolio/src/components/pages/Resume.js
@@ -1,53 +1,90 @@
 import React from "react";
+import { useState } from "react";
+import resume from "../assets/ryanResume.png";
+import email from "../assets/emailIcon.png";
+import github from "../assets/githubPng.png";
+import linkedIn from "../assets/linkedinicon.png";
 
 export default function Resume() {
-  const color = {
-    backgroundColor: "#9219E1",
+  const [isZoomed, setIsZoomed] = useState(false);
+
+  const cardContainerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    background:
+      "linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3), #fff)",
   };
-  // put image of resume down below
+
+  const cardStyle = {
+    margin: "0 10%",
+    boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
+    padding: "20px",
+    width: isZoomed ? "90%" : "80%",
+    transition: "width 0.3s",
+  };
+
+  const resumeImageStyle = {
+    marginBottom: "20px",
+    width: "100%",
+    height: "auto",
+    objectFit: isZoomed ? "cover" : "contain",
+    cursor: "pointer",
+    transition: "width 0.3s",
+    maxWidth: "100%",
+  };
+
+  const toggleZoom = () => {
+    setIsZoomed(!isZoomed);
+  };
+
+  const buttonGroupStyle = {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "20px",
+  };
+  const buttonStyle = {
+    width: "100px",
+    height: "100px",
+  };
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="text-center">
-            <h1>Contact Me</h1>
-            <p>Get in touch using the buttons below:</p>
-            <div className="button-group">
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="contact-button">
+    <div style={cardContainerStyle}>
+      <div className="container" style={{ width: "80%" }}>
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card text-center" style={cardStyle}>
+              <div className="card-body">
+                <div>
                   <img
-                    src="/path/to/github-icon.png"
-                    alt="GitHub"
-                    className="button-icon"
+                    style={resumeImageStyle}
+                    src={resume}
+                    alt="Resume"
+                    onClick={toggleZoom}
                   />
-                </button>
-              </a>
-              <a
-                href="https://linkedin.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="contact-button">
-                  <img
-                    src="/path/to/linkedin-icon.png"
-                    alt="LinkedIn"
-                    className="button-icon"
-                  />
-                </button>
-              </a>
-              <a href="mailto:your-email@example.com">
-                <button className="contact-button">
-                  <img
-                    src="/path/to/email-icon.png"
-                    alt="Email"
-                    className="button-icon"
-                  />
-                </button>
-              </a>
+                </div>
+                <h3>Contact Me</h3>
+                <p>Get in touch using the buttons below:</p>
+                <div style={buttonGroupStyle}>
+                  <a
+                    href="https://github.com/RyanWood2334"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={github} alt="GitHub" style={buttonStyle} />
+                  </a>
+                  <a
+                    href="https://linkedin.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={linkedIn} alt="LinkedIn" style={buttonStyle} />
+                  </a>
+                  <a href="mailto:ryanwood_2334@yahoo.com">
+                    <img src={email} alt="Email" style={buttonStyle} />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
